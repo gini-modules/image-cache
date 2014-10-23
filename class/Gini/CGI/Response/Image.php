24fe98640d64d68053eea7cd2c7fd26da63f6bc4
Hongjie Zhu <hongjie.zhu@geneegroup.com>
2014-10-23 09:29:40 +0800

    init

---
+++ class/Gini/CGI/Response/Image.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+* @file IMAGE.php
+* @brief Response Image
+* @author Hongjie Zhu
+* @version 0.1.0
+* @date 2014-10-21
+ */
+namespace Gini\CGI\Response;
+
+    class IMAGE
+    {
+        private $_content;
+        private $_type;
+
+        public function __construct($content, $type=null)
+        {
+            $this->_content = $content;
+            $this->_type = $type;
+        }
+
+        public function output()
+        {
+            switch ($this->_type) {
+                case 'image/png':
+                default:
+                    $type = 'image/png';
+            }
+            header("Content-Type: {$type}");
+            file_put_contents('php://output', (string) $this->_content);
+        }
+
+        public function content()
+        {
+            return $this->_content;
+        }
+
+    }
