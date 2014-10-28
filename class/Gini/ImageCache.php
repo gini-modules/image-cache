57ba4fac9c8d65b6efbc9bebe84170a1c013a6ed
Hongjie Zhu <hongjie.zhu@geneegroup.com>
2014-10-28 14:29:32 +0800

    ImageCache::makeUrl

---
+++ class/Gini/ImageCache.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @file File.php
+* @brief 图片处理
+* @author Hongjie Zhu
+* @version 0.1.0
+* @date 2014-10-21
+ */
+
+namespace Gini;
+
+class ImageCache
+{
+    public static function makeUrl($url, $size=null, $path=null, $format='png')
+    {
+        $config = (array)\Gini\Config::get('app.image_cache');
+        if (empty($config)) return $url;
+
+        $server = $config['server'];
+        if (empty($server)) return $url;
+
+        $client_id = $config['client_id'];
+        $client_secret = $config['client_secret'];
+
+        if (!$client_id || !$client_secret) return $url;
+
+        $hash = hash_hmac('md5', $url, $client_secret);
+
+        $result = vsprintf('%s%s%s%s.%s?url=%s&client_id=%s', [
+            rtrim($server, '/') . '/',
+            $path ? rtrim($path, '/') . '/' : '',
+            $hash,
+            $size ? '@' . $size : '',
+            $format,
+            urlencode($url),
+            urlencode($client_id)
+        ]);
+
+        return $result;
+    }
+}
