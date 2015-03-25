48e0c1cdd8558e20f8af099549d2e2bd6c9a2c4c
pihizi <pihizi@msn.com>
2015-03-25 11:21:10 +0800

    RPC的调用需要api path

--- class/Gini/Module/ImageCache.php
+++ class/Gini/Module/ImageCache.php
@@ -11,9 +11,10 @@ class ImageCache
     public static function diagnose()
     {
         // check image-cache: authorize
-        if ('image-cache'!==end(array_keys(\Gini\Core::$MODULE_INFO))) {
+        $keys = array_keys(\Gini\Core::$MODULE_INFO);
+        if ('image-cache'!==end($keys)) {
             $conf = \Gini\Config::get('app.image_cache');
-            $rpc = \Gini\IoC::construct('\Gini\RPC', $conf['server']);
+            $rpc = \Gini\IoC::construct('\Gini\RPC', rtrim($conf['server'], '/').'/api');
             if (!$rpc->imagecache->authorize($conf['client_id'], $conf['client_secret'])) {
                 return ['Please check your image_cache rpc config in app.yml!'];
             }
