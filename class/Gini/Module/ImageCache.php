ba1b0048da56d0cd8d14d2351c5ec2f33d28d66e
pihizi <pihizi@msn.com>
2015-04-01 10:49:18 +0800

    gini doctor try...catch..

--- class/Gini/Module/ImageCache.php
+++ class/Gini/Module/ImageCache.php
@@ -14,8 +14,13 @@ class ImageCache
         $keys = array_keys(\Gini\Core::$MODULE_INFO);
         if ('image-cache'!==end($keys)) {
             $conf = \Gini\Config::get('app.image_cache');
-            $rpc = \Gini\IoC::construct('\Gini\RPC', rtrim($conf['server'], '/').'/api');
-            if (!$rpc->imagecache->authorize($conf['client_id'], $conf['client_secret'])) {
+            try {
+                $rpc = \Gini\IoC::construct('\Gini\RPC', rtrim($conf['server'], '/').'/api');
+                if (!$rpc->imagecache->authorize($conf['client_id'], $conf['client_secret'])) {
+                    return ['Please check your image_cache rpc config in app.yml!'];
+                }
+            }
+            catch (\Exception $e) {
                 return ['Please check your image_cache rpc config in app.yml!'];
             }
         }
