37ae5ef7d025efe05cae6d489a4b124c705f60fb
pihizi <pihizi@msn.com>
2015-04-08 17:23:16 +0800

    从app的doctor.yml获取需要注册的image sizes和paths信息，进行检测

--- class/Gini/Module/ImageCache.php
+++ class/Gini/Module/ImageCache.php
@@ -19,6 +19,18 @@ class ImageCache
                 if (!$rpc->imagecache->authorize($conf['client_id'], $conf['client_secret'])) {
                     return ['Please check your image_cache rpc config in app.yml!'];
                 }
+                $opts = \Gini\Config::get('doctor.image_cache');
+                if (!$opts) {
+                    return ['Please check your image_cache config in doctor.yml!'];
+                }
+                $sizes = $opts['sizes'];
+                if (is_array($sizes) && !$rpc->imagecache->hasSizes($sizes)) {
+                    return ['image-cache: not all image sizes are registered!'];
+                }
+                $paths = $opts['paths'];
+                if (is_array($paths) && !$rpc->imagecache->hasPaths($paths)) {
+                    return ['image-cache not all image paths are registered!'];
+                }
             }
             catch (\Exception $e) {
                 return ['Please check your image_cache rpc config in app.yml!'];
